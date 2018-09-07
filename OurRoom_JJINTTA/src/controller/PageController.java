da92e6766d7cf25174f78132b7f703147f2aed02
hanna <hanna@gmail.com>
2018-09-07 11:32:34 +0900

    xx

--- OurRoom_JJINTTA/src/controller/PageController.java
+++ OurRoom_JJINTTA/src/controller/PageController.java
@@ -114,7 +114,7 @@ public class PageController {
 		return mav;
 	}
 	
-	@RequestMapping("/project/kanban2")
+	@RequestMapping("/project/kanban")
 	public ModelAndView project_kanban(int pNum, int tNum) {
 		
 		ModelAndView mav = new ModelAndView();
@@ -157,7 +157,7 @@ public class PageController {
 		return mav;
 		
 	}
-	@RequestMapping("/project/kanban")
+	@RequestMapping("/project/kanban2")
 	public ModelAndView project_kanban2(int pNum, int tNum) {
 		
 		ModelAndView mav = new ModelAndView();
